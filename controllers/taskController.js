936a61c83ce6c959a33664377b10fca671be56aa
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-20 00:45:26 +0100

    CRUD operations are added

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,9 +2,46 @@ const { StatusCodes } = require("http-status-codes");
 
 const Task = require("../models/userModel");
 
-exports.getTasks = async (req, res, next) => {
+exports.getAllTasks = async (req, res, next) => {
   try {
     const tasks = await Task.find();
     res.status(StatusCodes.OK).json({ tasks: tasks });
-  } catch (error) {}
+  } catch (err) {
+    const error = new Error("Can't find all jobs");
+    error.StatusCode = StatusCodes.NOT_FOUND;
+    throw error;
+  }
+};
+
+exports.createTask = async (req, res, next) => {
+  const task = new Task(req.body);
+  await task.save();
+
+  res.status(StatusCodes.CREATED).json({ task: task });
+};
+
+exports.deleteTask = async (req, req, next) => {
+  const { id } = req.params;
+  const removedJob = await Task.findByIdAndDelete(id);
+
+  if (!removedJob) {
+    const error = new Error(
+      "Job not found for providing id in order to delete"
+    );
+    error.StatusCode = StatusCodes.NOT_FOUND;
+    throw error;
+  }
+  res.status(StatusCodes.OK).json({ message: "job deleted", job: removedJob });
+};
+exports.deleteAllCompletedTasks = async (req, req, next) => {
+  try {
+    await Task.findByIdAndDelete({ completed: true });
+    res
+      .status(StatusCodes.OK)
+      .json({ message: "completed jobs deleted", job: removedJob });
+  } catch (err) {
+    const error = new Error("Fail to delete all completed jobs");
+    error.StatusCode = StatusCodes.NOT_FOUND;
+    throw error;
+  }
 };
