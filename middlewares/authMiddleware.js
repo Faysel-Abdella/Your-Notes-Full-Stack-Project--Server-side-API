6461bab549813536e8cfd426b60c4cdb053cb6ed
faysel abdella <fayselcode@gmail.com>
2023-08-28 07:36:01 -0700

    token authentication method changed from cookie to localstorage

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,10 +5,7 @@ exports.authenticateUser = (req, res, next) => {
   // ** Step 1: Check if token cookie exist from incoming request cookies,
   //    if it does't exist response unauthenticated error
 
-  //Since you install the cookie-parser package, you can access the cookie form incoming request, just
-  //by saying req.cookie
-  console.log(req);
-  const { token } = req.cookies;
+  const token = req.headers.authorization.split(" ")[1];
   if (!token) {
     const error = new Error("unauthenticated error, token does't exist");
     error.statusCode = StatusCodes.UNAUTHORIZED;
