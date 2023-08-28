bb8c6756e85f80b0285ad325b332630046e9a6d0
faysel abdella <fayselcode@gmail.com>
2023-08-28 07:54:08 -0700

    token accepting isuue

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ exports.authenticateUser = (req, res, next) => {
   // ** Step 1: Check if token cookie exist from incoming request cookies,
   //    if it does't exist response unauthenticated error
 
-  const token = req.headers.Authorization.split(" ")[1];
+  const token = req.headers.Authorization;
   if (!token) {
     const error = new Error("unauthenticated error, token does't exist");
     error.statusCode = StatusCodes.UNAUTHORIZED;
